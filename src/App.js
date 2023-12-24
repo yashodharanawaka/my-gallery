bb610eb58f22841078bab2468b2d9f50a3b85701
Yashodha Ranawaka <yashodharanawaka@gmail.com>
2023-12-24 18:22:48 +0530

    added a nav bar

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 import PhotoGallery from './components/PhotoGallery';
 import Lightbox from './components/Lightbox';
 import MyProfile from "./components/MyProfile";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
     return (
         <Router>
+            <NavigationBar/>
             <Routes>
-                <Route path="/photo/:id" element={<Lightbox />} />
-                <Route path="/profile" element={<MyProfile />} />
-                <Route path="/*" element={<PhotoGallery />} />
+                <Route path="/photo/:id" element={<Lightbox/>}/>
+                <Route path="/profile" element={<MyProfile/>}/>
+                <Route path="/*" element={<PhotoGallery/>}/>
             </Routes>
         </Router>
     );
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const NavigationBar = () =>{
+    return <nav>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/profile">Profile</Link>
+            </li>
+        </ul>
+    </nav>
+}
+
+export default NavigationBar;
