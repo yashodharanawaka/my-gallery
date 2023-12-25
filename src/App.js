c7b6b26bf00ac4b061fe3e6d18f19af071e9d58c
Yashodha Ranawaka <yashodharanawaka@gmail.com>
2023-12-25 19:07:26 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import PhotoGallery from './components/PhotoGallery';
 import Lightbox from './components/Lightbox';
